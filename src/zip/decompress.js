29023efdf3721d4efa1033d2c5df9a1c05e90dfa
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 18:22:35 +0200

    feat: finish zip.decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from 'fs'
+import path from 'path';
+import zlib from 'zlib';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToCompress1.txt');
+const archive = path.join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+    const gzip = zlib.createUnzip();
+    const readableStream = fs.createReadStream(archive);
+    const writableStream = fs.createWriteStream(file);
+    readableStream.pipe(gzip).pipe(writableStream); 
 };
 
 await decompress();
