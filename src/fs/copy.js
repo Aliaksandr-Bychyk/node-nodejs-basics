f31cfc8a2c94f64de995d3653ac1d43dab0d04c3
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 14:52:12 +0200

    feat: finish fs.copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import fs from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const startFolder = path.join(__dirname, 'files');
+const endFolder = path.join(__dirname, 'copy-files');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.access(startFolder)
+        try {
+            await fs.access(endFolder)
+            throw new Error('FS operation failed')
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                fs.cp(startFolder, endFolder, { recursive: true })
+            } else {
+                throw err
+            }
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        } else {
+            throw err
+        }
+    }
 };
 
 await copy();
