82f9db9823915253ff1e21acb759f9bf39372f28
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 15:15:33 +0200

    feat: finish fs.delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import fs from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.access(file)
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        }
+    }
+    await fs.unlink(file)
 };
 
 await remove();
