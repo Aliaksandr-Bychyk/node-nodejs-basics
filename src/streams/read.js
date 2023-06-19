251d5fb93ea9c86a4eca03fbd1bce65151f53dd8
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 17:45:18 +0200

    feat: finish stream.read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(file, 'utf-8');
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
