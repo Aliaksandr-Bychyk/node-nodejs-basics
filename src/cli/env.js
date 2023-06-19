ee34bf18509bd4e2d4618fbe910279464ae8ef95
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 15:39:58 +0200

    feat: finish cli.env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    console.log(
+        Object.entries(process.env)
+            .filter(value => value[0].startsWith('RSS_'))
+            .map(value => `${value[0]}=${value[1]}`).join('; ')
+    )
 };
 
 parseEnv();
