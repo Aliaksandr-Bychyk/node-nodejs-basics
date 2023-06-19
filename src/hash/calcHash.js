a275d40650f8824734f3dc977ecb94d724a3977c
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 17:15:34 +0200

    feat: finish hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import crypto from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    hash.update(file);
+    console.log(hash.digest('hex'))
 };
 
 await calculateHash();
