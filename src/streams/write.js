b189625792bfe609bf1302c2e2f885afc83b984a
Aliaksandr <sanya55bychyk@gmail.com>
2023-06-19 18:00:53 +0200

    feat: finish stream.write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = fs.createWriteStream(file);
+    process.stdin.pipe(writableStream);
 };
 
 await write();
